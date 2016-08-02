13f54204bc56cc66350f7b7588e8a4ea37fbd743
averypp <pp1010687372@126.com>
2016-08-02 15:10:47 +0800

    up

--- upload/conf/database.php
+++ upload/conf/database.php
@@ -2,7 +2,7 @@
   return array(
 	'dsn' => 'mysql:host=localhost;dbname=phpwind;port=3306',
 	'user' => 'root',
-	'pwd' => 'root',
+	'pwd' => '266024',
 	'charset' => 'utf8',
 	'tableprefix' => 'pw_',
 	'engine' => 'MyISAM',
